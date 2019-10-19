3c1d1f5ca4e6b4119238f2fd3a60fcd8951216a4
LeonAdeoye <leon.adeoye@gmail.com>
2019-10-19 22:55:06 +0800

    fixed typo

--- src/main/java/com/leon/services/MessagingService.java
+++ src/main/java/com/leon/services/MessagingService.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 public interface MessagingService
 {
-    public void saveConfiguration(Configuration configfuration);
+    public void saveConfiguration(Configuration configuration);
     public List<Configuration> loadAllConfigurations();
 }
