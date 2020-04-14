122a37ee0b02623c1fe17ab28c8a9538230abb8c
LeonAdeoye <leon.adeoye@gmail.com>
2020-04-14 20:07:09 +0800

    updated configuration service implementation

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,12 @@
 package com.leon;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-import java.util.Arrays;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application
 {
     public static void main(String[] args)
--- src/main/java/com/leon/services/ConfigurationServiceImpl.java
+++ src/main/java/com/leon/services/ConfigurationServiceImpl.java
@@ -1,33 +1,25 @@
 package com.leon.services;
-
 import com.leon.models.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class ConfigurationServiceImpl implements ConfigurationService
 {
     private static final Logger logger = LoggerFactory.getLogger(ConfigurationServiceImpl.class);
-
-    // TODO: change component name
-    private static final String THIS_COMPONENT_NAME = "SpringBootQuickStart";
     private static Map<String, Map<String,List<Configuration>>> configurations;
-
+    // TODO: set micro-service name
+    private static final String THIS_COMPONENT_NAME = "THIS_COMPONENT_NAME";
     @Autowired
     private MessagingService messagingService;
 
-
     public ConfigurationServiceImpl()
     {
-
     }
 
     @PostConstruct
@@ -45,15 +37,6 @@ public class ConfigurationServiceImpl implements ConfigurationService
         return this.configurations.get(key).get(owner).get(0).getValue();
     }
 
-    @Override
-    public void saveConfiguration(String owner, String key, String value)
-    {
-        Configuration configuration = new Configuration(owner, key, value);
-        logger.info("Saving configuration: ", configuration);
-        this.messagingService.saveConfiguration(configuration);
-    }
-
-    @Override
     public void loadAllConfigurations()
     {
         List<Configuration> loadedConfigurations = this.messagingService.loadAllConfigurations();
@@ -64,7 +47,6 @@ public class ConfigurationServiceImpl implements ConfigurationService
     @Override
     public void reconfigure()
     {
-        logger.info("Reconfiguring...");
         this.loadAllConfigurations();
     }
 }
